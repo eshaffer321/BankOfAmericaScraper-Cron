0a665bbad80ac42ae303cd471e05c3077c81c481
Erick Shaffer <erick.shaffer321@gmail.com>
2019-08-07 08:15:30 -0700

    updated schedule

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ new CronJob('00 00 01 * * *', function() {
     request('http://localhost:5000');
 }, null, true, 'America/Los_Angeles');
 
-new CronJob('* * * * * *', function() {
+new CronJob('00 00 00 * * *', function() {
     request('http://localhost:80/update-date');
 }, null, true, 'America/Los_Angeles');
